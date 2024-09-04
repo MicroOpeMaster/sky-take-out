db1c8cd70a743120f9e974e690299b3b7febbd2b
MicroOpeMaster <944838321@qq.com>
2024-09-04 16:42:12 +0800

    新增菜品业务代码

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,5 +1,10 @@
 package com.sky.mapper;
 
+import com.sky.annotation.Autofill;
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -14,4 +19,6 @@ public interface DishMapper {
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
+    @Autofill(value = OperationType.INSERT)
+    void insert(Dish dish);
 }
